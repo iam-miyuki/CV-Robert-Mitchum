df8cd28743fefe20980277f612ee6c1fa4651a50
Miyuki <miyuki@mail.com>
2025-03-19 21:57:41 +0100

    update js :white_check_mark:

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// // function changeJumbotronTitle
+// function changeJumbotronTitle
 const buttonBlue = document.querySelector('#welcome .btn')
 buttonBlue.textContent="Changer le titre"
 buttonBlue.removeAttribute("href");
@@ -8,22 +8,20 @@ const jumbotron = document.querySelector('#welcome .display-1');
 function changeJumbotronTitle(){
         jumbotron.textContent = "Youhou"
         buttonBlue.textContent= "Contactez-moi"
-        // buttonBlue.setAttribute("href","contact.html")
-        
 }
 buttonBlue.onclick = changeJumbotronTitle;
 
-
-const competences = document.querySelectorAll("#competence p");
+// function changeCompetence
+const competence = document.querySelectorAll("#competence p");
 const titleCompetence = document.querySelector("#competence h2");
 const logos = document.querySelectorAll("#competence img");
-const tabloP = [
+const paragraphs = [
   "L'HyperText Markup Language, généralement abrégé HTML, est le langage de balisage conçu pour représenter les pages web.",
   "Les feuilles de style en cascade, généralement appelées CSS de l'anglais Cascading Style Sheets, forment un langage informatique qui décrit la présentation des documents HTML et XML. Les standards définissant CSS sont publiés par le World Wide Web Consortium.",
   "JavaScript est un langage de programmation de scripts principalement employé dans les pages web interactives mais aussi pour les serveurs avec l'utilisation de Node.js.",
   "AngularJS est un framework JavaScript libre et open source développé par Google. Il permet de développer des pages web",
 ];
-const tabloImg = [
+const imgs = [
   "img/logos/html.png",
   "img/logos/css.png",
   "img/logos/js.png",
@@ -31,9 +29,9 @@ const tabloImg = [
 ];
 
 function changeCompetence() {
-  for (let i = 0; i < competences.length; i++) {
-    competences[i].textContent = tabloP[i];
-    logos[i].src = tabloImg[i];
+  for (let i = 0; i < competence.length; i++) {
+    competence[i].textContent = paragraphs[i];
+    logos[i].src = imgs[i];
   }
 }
 titleCompetence.onclick = changeCompetence;
